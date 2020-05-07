5b2cbf3fa52fd9b197edfa1e3a654453b1cdc1b3
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-07 22:18:32 +0000

    Done.

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ class Circle {
         return this.radius * 2
     }
     
+    get circumference() {
+        return this.radius *2 * Math.PI
+    }
+    
+    get area() {
+        return Math.PI *(this.radius ** 2)
+    }
+    
+    set diameter(diam) {
+        this.radius =  diam/2
+    }
+    
+    set circumference(circ) {
+        this.radius = circ/(2*(Math.PI))
+    }
 }
